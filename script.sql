7a89d4f62820ccaf29a14f3beddc62f3aae2a9f7
pblo <pdiazarenas541@gmail.com>
2026-02-24 15:07:31 +0000

    first commit

---
+++ script.sql
@@ -0,0 +1,15 @@
+-- Active: 1769730037726@@127.0.0.1@5432@futscript
+CREATE DATABASE futscript;
+\c futscript;
+
+CREATE TABLE equipos (id SERIAL PRIMARY KEY, name VARCHAR(250) NOT NULL);
+
+CREATE TABLE posiciones (id SERIAL PRIMARY KEY, name VARCHAR(250) NOT NULL);
+
+CREATE TABLE jugadores (id SERIAL PRIMARY KEY, id_equipo INT REFERENCES equipos(id), name VARCHAR(250), position INT REFERENCES posiciones(id));
+
+INSERT INTO posiciones values
+(DEFAULT, 'delantero'),
+(DEFAULT, 'centrocampista'),
+(DEFAULT, 'defensa'),
+(DEFAULT, 'portero');
